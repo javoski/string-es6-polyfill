bf856efd261a40651c002314aeb923404fa5cdd1
xujiongbo <xujiongbo@gmail.com>
2016-06-30 20:00:18 +0800

    add method repeat

--- string-es6-polyfill.js
+++ string-es6-polyfill.js
@@ -5,30 +5,59 @@
         //...
     }
     proto.includes = proto.includes || function(sub){
-        if(typeof sub !== "string") {
+        if(typeof sub !== "string"){
             throw new TypeError("Parameter should be a string")
-            return
         }
         return (new RegExp(sub)).test(this)
     }
     proto.startsWith = proto.startsWith || function(sub){
-        if(typeof sub !== "string") {
+        if(typeof sub !== "string"){
             throw new TypeError("Parameter should be a string")
-            return
         }
         return (new RegExp('^' + sub)).test(this)
     }
     proto.endsWith = proto.endsWith || function(sub){
-        if(typeof sub !== "string") {
+        if(typeof sub !== "string"){
             throw new TypeError("Parameter should be a string")
-            return
         }
         return (new RegExp(sub + '$')).test(this)
     }
     proto.repeat = proto.repeat || function(count){
-        if(typeof count !== "number") {
+        if(typeof count !== "number"){
             throw new TypeError("Parameter should be a number")
-            return
+        }else if (count < 0){
+            throw new RangeError('Repeat count must be non-negative');
+        }else if (count === Infinity){
+            throw new RangeError('Repeat count must be less than infinity');
+        }
+        var str = '' + this
+        count = Math.floor(count)
+        if(this.length === 0 || count === 0){
+            return ''
+        }
+        var res = str
+        while(true){
+            if((count & 1) === 1){
+                res += str
+            }
+            count >>>= 1
+            if(count === 0){
+                break
+            }
+            str += str
+        }
+        return res
+    }
+    proto.padStart = proto.padStart || function(len, filler){
+        if(typeof len !== "number"){
+            throw new TypeError("First parameter should be a number")
+        }
+
+        //...
+    }
+    proto.padEnd = proto.padEnd || function(len, filler){
+        if(typeof len !== "number"){
+            throw new TypeError("First parameter should be a number")
         }
         //...
     }
